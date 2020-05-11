e3f4ddc13615464ced99ee00e15c74b77379e903
David Petnick <dpetnick89@gmail.com>
2020-05-11 20:19:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-def display_rainbow(colors = rainbow)
+def display_rainbow(colors)
   colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
   puts "R: #{colors[0]}"
   puts "O: #{colors[1]}"
